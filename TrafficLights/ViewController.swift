af9d03c7c0d48826212de5d6197396498cf36955
JuliDeveloper <rja-97@yandex.ru>
2022-04-05 22:05:23 +0300

    Added logic for switching colors

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -14,14 +14,50 @@ class ViewController: UIViewController {
     @IBOutlet var greenLightsView: UIView!
     @IBOutlet var toggleButton: UIButton!
     
+    let minAlpha = 0.3
+    let maxAlpha = 1.0
+    let circleRadius = 50.0
+    let buttonRadius = 10.0
+    
+    var colorTagState = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        redLightsView.alpha = minAlpha
+        yellowLightsView.alpha = minAlpha
+        greenLightsView.alpha = minAlpha
         
+        redLightsView.layer.cornerRadius = circleRadius
+        yellowLightsView.layer.cornerRadius = circleRadius
+        greenLightsView.layer.cornerRadius = circleRadius
+        toggleButton.layer.cornerRadius = buttonRadius
+        
+        toggleButton.setTitle("START", for: .normal)
     }
 
     @IBAction func switchTrafficLights() {
-        
+        toggleButton.setTitle("NEXT", for: .normal)
+                
+        switch colorTagState {
+            
+        case redLightsView.tag:
+            redLightsView.alpha = maxAlpha
+            greenLightsView.alpha = minAlpha
+            colorTagState = yellowLightsView.tag
+            
+        case yellowLightsView.tag:
+            yellowLightsView.alpha = maxAlpha
+            redLightsView.alpha = minAlpha
+            colorTagState = greenLightsView.tag
+            
+        case greenLightsView.tag:
+            greenLightsView.alpha = maxAlpha
+            yellowLightsView.alpha = minAlpha
+            colorTagState = redLightsView.tag
+            
+        default:
+            break
+        }
     }
-    
 }
 
