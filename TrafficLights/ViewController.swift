5df9c2fd4a07ecb0731e275bf27909236d49a135
JuliDeveloper <rja-97@yandex.ru>
2022-04-05 20:47:25 +0300

    Added IBOutlets and IBAction

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightsView: UIView!
+    @IBOutlet var yellowLightsView: UIView!
+    @IBOutlet var greenLightsView: UIView!
+    @IBOutlet var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func switchTrafficLights() {
+        
+    }
+    
 }
 
